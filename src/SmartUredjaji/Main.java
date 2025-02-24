a8c4a78f1a68db091d8f3e63557eb88b15e1356b
Ines <ines.cvek16@gmail.com>
2025-02-24 02:10:33 +0100

    finished SmartThermostat

--- src/SmartUredjaji/Main.java
+++ src/SmartUredjaji/Main.java
@@ -2,24 +2,41 @@ package SmartUredjaji;
 
 public class Main {
     public static void main(String[] args) {
-        SmartLight light = new SmartLight("1");
-        SmartThermostat thermo = new SmartThermostat("1");
-        SmartSpeaker speaker = new SmartSpeaker("1");
+        SmartLight light = new SmartLight();
+        SmartThermostat thermo = new SmartThermostat();
+        SmartSpeaker speaker = new SmartSpeaker();
 
         //LIGHT AKCIJE
-        System.out.println("Light: ");
+        System.out.println("\nLight: ");
+        System.out.println(light.displayStatus());
+        System.out.println(light.performAction());
+        light.remoteControl("pojačaj");
+        light.remoteControl("pojačaj");
 
         //THERMOSTAT AKCIJE
-        System.out.println("Thermo: ");
+        System.out.println("\nThermo: ");
+        System.out.println(thermo.displayStatus());
+        System.out.println(thermo.performAction());
+        thermo.remoteControl("smanji");
+        thermo.remoteControl("smanji");
 
         //SPEAKER AKCIJE
-        System.out.println("Speaker: ");
+        System.out.println("\nSpeaker: ");
+        System.out.println(speaker.displayStatus());
+        System.out.println(speaker.performAction());
+        speaker.remoteControl("pojačaj");
 
         //UPDATE VERZIJE
-        System.out.println("Update uređaja:");
-        light.updateFirmware("2");
-        thermo.updateFirmware("2");
-        speaker.updateFirmware("2");
+        System.out.println("\nUpdate uređaja:");
+        light.updateFirmware("2.0");
+        thermo.updateFirmware("2.0");
+        speaker.updateFirmware("2.0");
+
+        //STATUSI NAKON SVIH PROMJENA
+        System.out.println("\nStatusi nakon svih promjena:");
+        System.out.println(light.displayStatus());
+        System.out.println(thermo.displayStatus());
+        System.out.println(speaker.displayStatus());
 
     }
 }
--- src/SmartUredjaji/SmartThermostat.java
+++ src/SmartUredjaji/SmartThermostat.java
@@ -1,22 +1,52 @@
 package SmartUredjaji;
 
+import java.util.Scanner;
+
 public class SmartThermostat extends SmartDevice implements RemoteControllable {
-    public SmartThermostat(String version) {
+    private double temp;
+
+    public SmartThermostat(){
+        this("1.0", 30.0);
+    }
+
+    public SmartThermostat(String version){
+        this(version, 0.0);
+    }
+
+    public SmartThermostat(String version, double temp) {
         super(version, "SmartThermostat");
+        this.temp = temp;
     }
 
     @Override
     public void remoteControl(String command) {
-
+        switch (command) {
+            case "pojačaj" -> this.temp += 1.0;
+            case "smanji" -> this.temp -= 1.0;
+            default -> System.out.println("Nisam razumio. SmartThermostat ostaje isti.");
+        }
     }
 
     @Override
     public String performAction() {
-        return "";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Unesi novu temperaturu:");
+        try {
+            double temp = sc.nextDouble();
+            if (temp>=10 && temp<40) {
+                this.temp = temp;
+            } else {
+                System.out.println("Nevažeći unos. Temperatura ostaje ista.");
+            }
+        } catch (Exception e) {
+            System.out.println("Nevažeći unos. Temperatura ostaje ista.");
+            sc.nextLine();
+        }
+        return displayStatus();
     }
 
     @Override
     public String displayStatus() {
-        return "";
+        return naziv + " - " + "Temperatura: " + temp + "°C";
     }
 }
